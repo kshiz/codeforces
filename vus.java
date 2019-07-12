6492cd897388d8ddc3f185f910350131850b3cb2
Kshiz <tkshitij815@gmail.com>
2019-07-12 12:13:22 +0530

    1186A

---
+++ vus.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class vus{
+  public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int n=sc.nextInt();
+    int m=sc.nextInt();
+    int k=sc.nextInt();
+    if(n<=m&&n<=k){
+      System.out.println("Yes");
+    }
+    else{
+      System.out.println("No");
+    }
+  }
+}
