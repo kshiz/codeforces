977f8be9afc709954d12c8ee5d0bf2c94541c39b
Kshiz <tkshitij815@gmail.com>
2019-07-16 15:20:33 +0530

    1194 A

---
+++ progression.java
@@ -0,0 +1,15 @@
+//1194A
+import java.util.*;
+public class progression{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t!=0){
+      int n=sc.nextInt();
+      int x=sc.nextInt();
+      System.out.println(2*x);
+      t--;
+    }
+
+  }
+}
