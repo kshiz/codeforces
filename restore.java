c4dc81e56ddf40d829c756b0dc472fb9c4e6e6e5
Kshiz <tkshitij815@gmail.com>
2019-07-18 16:20:37 +0530

    1154 A

---
+++ restore.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class restore{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a[]=new int[4];
+    for(int i=0;i<4;i++){
+      a[i]=sc.nextInt();
+    }
+    Arrays.sort(a);
+    System.out.println((a[3]-a[0])+" "+(a[3]-a[1])+" "+(a[3]-a[2]));
+  }
+}
